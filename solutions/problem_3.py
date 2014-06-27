8d663c5d172c976b9d54013b707e127a91258dcc
Joss <dylajoss@406c8f534be4.ant.amazon.com>
2014-06-27 10:43:27 -0700

    remove sieve from problem_3

--- solutions/problem_3.py
+++ solutions/problem_3.py
@@ -4,30 +4,6 @@
 #
 # What is the largest prime factor of the number 600851475143 ?
 
-from math import sqrt
-import time
-
-# unused for this problem, but here it is
-def sieve(limit):
-    """
-    generate all primes less than limit
-    limit: int
-    """
-    candidates = [True]*limit # after the algorithm concludes candidate[i] = True iff i is prime
-    candidates[0] = candidates[1] = False
-    res = []
-    
-    for i in range(2, int(sqrt(limit))):
-        if candidates[i]:
-            for j in range(i**2, limit, i):
-                candidates[j] = False
-
-    for num, isPrime in enumerate(candidates):
-        if isPrime:
-            res.append(num)
-
-    return res
-
 def largest_prime_factor(number):
     current = 2
     while current < number:
